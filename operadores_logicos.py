cee8d92c13841b4b6d10354ad415f5a6f4cf1c8d
Mac de Lu <namor1901@gmail.com>
2025-03-07 09:30:53 -0500

    operadores_logicos.py

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -1,5 +1,24 @@
-'''Tablas de verdad en python'''
+'''Tablas de verdad en python 
+JERARQUIA DE OPERADORES
+1.()
+2.**
+3.+ -
+4.* / %
+5.> < != == >== <==
+6.NOT
+7.AND
+8.OR
+9. =
+'''
 op1 = True
 op2 = True
-op3 = op1 and p2
+op3 = op1 and op2
 print(op3)
+
+op4 = not op2
+
+op1 = False
+op2 = True
+op3 = False
+op4 = True
+resultado = not op1 and (op2 or op3 and not op1) and not op4
