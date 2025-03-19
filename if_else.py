0df0ba4afd4a40cd2dcd86139243573b3e432ebe
Mac de Lu <namor1901@gmail.com>
2025-03-19 09:26:16 -0500

    If else

--- if_else.py
+++ if_else.py
@@ -1,6 +1,6 @@
-edad = 19 
-documento = False
-if edad >= 18 and documento == True:
+edad = int(input("Ingrese su edad: "))
+documento = input("Tiene documento di/do: ")
+if edad >= 18 and documento == "di":
     print ("Ud es mayor de edad")
     print ("Puede votar")
 else:
