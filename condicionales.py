714e78e0d5a17582617bb1199f3411ada094ff7a
Mac de Lu <namor1901@gmail.com>
2025-03-19 07:47:26 -0500

    if_else

---
+++ condicionales.py
@@ -0,0 +1,12 @@
+'''Ejemplo de condicional'''
+A = 3
+B = 2
+C = 1 
+D = 4
+resultado = not (A ** 2-B > C ** 2) and ((a* 3 + C) / 2 < D ) or True
+print (resultado)
+
+X = 5
+Y = 10
+Z = 5
+resultado = (X == Z +(8 / Z))and not ((Y+3)*(4 / (Z + 1))) == Z 
---
+++ if_else.py
@@ -0,0 +1,10 @@
+edad = 19 
+documento = False
+if edad >= 18 and documento == True:
+    print ("Ud es mayor de edad")
+    print ("Puede votar")
+else:
+    print("Ud es menor de edad")
+    print("o")
+    print("No puede votar")
+print("Fin del programa")
